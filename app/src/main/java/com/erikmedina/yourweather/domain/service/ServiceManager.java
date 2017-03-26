4b4698acde222c19598530f8b7cc3e4c5bf7523f
ErikMedina <ecmc85@gmail.com>
2017-03-26 17:40:25 +0200

    Add interceptors

--- app/src/main/java/com/erikmedina/yourweather/domain/service/ServiceManager.java
+++ app/src/main/java/com/erikmedina/yourweather/domain/service/ServiceManager.java
@@ -1,6 +1,9 @@
 package com.erikmedina.yourweather.domain.service;
 
 import com.erikmedina.yourweather.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,9 +14,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ServiceManager {
 
   public static MyApiEndpointInterface createWebService() {
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    logging.setLevel(Level.BODY);
+    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    httpClient.addInterceptor(logging);
+
     Retrofit retrofit = new Retrofit.Builder()
         .baseUrl(BuildConfig.API_ENDPOINT)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient.build())
         .build();
     return retrofit.create(MyApiEndpointInterface.class);
   }
